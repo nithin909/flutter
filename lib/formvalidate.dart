806e767461221fe7e77c201acad95e0212562911
ASUS <nithinthomas909@gmail.com>
2022-06-13 13:05:12 +0530

    commit

--- lib/formvalidate.dart
+++ lib/formvalidate.dart
@@ -1,24 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'contactlist.dart';
+
+void main(){
+  runApp(MaterialApp(
+      home:FormValidate()
+  ));
+}
 
 class FormValidate extends StatelessWidget{
   final form = GlobalKey<FormState>(); //for storing form state.
 
   //saving form after validation
-  void _saveform(){
-    final isValid = form.currentState!.validate();
-  }
+  //void _saveform(){
+   // final isValid = form.currentState!.validate();
+ // }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return  Scaffold(
         body:Form(
             key: form,
             child: ListView(
-              c
+              children: [
+                TextFormField(
+                  validator: (text){
+                    if (text == null|| !(text.contains('@')) || text.isEmpty ){
+                      return "Enter a valied Email!";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+                TextFormField(
+                  validator: (text){
+                    if (text == null || !(text.length <= 5) || text.isEmpty){
+                      return "Enter a valid password with 5 charectors";
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'password',
+                  ),
+                ),
+                ElevatedButton(onPressed: (){
+                  final isValid = form.currentState!.validate();
+                  if(isValid){
+                    Navigator.push(context,
+                        MaterialPageRoute(builder:
+                            (context) => ContactList()
+                        )
+                    );
+                  }else{
+                      Fluttertoast.showToast(
+                          msg:"Email or password incorrect",
+                          fontSize: 15,
+                          gravity: ToastGravity.TOP,
+                        backgroundColor: Colors.orange
+                      );
+                  }
+                }, child:Text("Submit")),
+                TextButton(onPressed: (){
+
+                },
+                    child: Text("Not a user register here")
+                )
+              ],
             )
         ),
-      ),
-    );
+      );
+
   }
 
 }
