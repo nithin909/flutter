7efdbf3693dadd95ec6411aa3136c24f7aba4d54
ASUS <nithinthomas909@gmail.com>
2022-06-21 13:16:46 +0530

    commit

---
+++ lib/bottom navigation/BasicBottomNav.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(BasicNavbar());
+}
+
+class BasicNavbar extends StatefulWidget{
+  @override
+  State<BasicNavbar> createState() => _BasicNavbarState();
+}
+
+class _BasicNavbarState extends State<BasicNavbar> {
+  int _selectedindex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.call,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+    Icon(
+      Icons.settings,
+      size: 150,
+    ),
+  ];
+  void _onItemTapped(int index){
+    setState(
+        (){
+          _selectedindex = index;
+        }
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title :Text("Bottom NavBar demo")
+          ),
+          body: ,
+        ),
+      );
+  }
+}
--- lib/listview/listview_builder.dart
+++ lib/listview/listview_builder.dart
@@ -4,14 +4,33 @@ void main(){
   runApp(Clistview());
 }
 
-class Clistview extends StatelessWidget{
-  final titles = ["Delhi", "London", "Vancouver","New York"];
-  final subtitles = [
-    "India Pouplation 19 million",
-    "Britain Population 8 million",
-    "Canada population 2.4 million"
-    "USA population 8.1 million"
+class Clistview extends StatefulWidget{
+  @override
+  State<Clistview> createState() => _ClistviewState();
+}
+
+class _ClistviewState extends State<Clistview> {
+  final List<String> titles = ["Delhi", "London", "Vancouver","New York"];
+
+  final List<String>  subtitles = [
+    "India ",
+    "Britain",
+    "Canada",
+    "USA "
   ];
+  final List<String> subtitles2 = [
+    "Pouplation 19 million",
+    "Pouplation 8 million",
+    "Pouplation 2.4 million",
+    "Pouplation 8.1 million"
+  ];
+  List<String> images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtC-DNAGGQBwADmleukYt8GK0J_Waweys_iQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtC-DNAGGQBwADmleukYt8GK0J_Waweys_iQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtC-DNAGGQBwADmleukYt8GK0J_Waweys_iQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtC-DNAGGQBwADmleukYt8GK0J_Waweys_iQ&usqp=CAU",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,15 +40,20 @@ class Clistview extends StatelessWidget{
           title: Text("Cities around world",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white),),
         ),
         body: ListView.builder(
-          itemCount: titles.length -1,
+          itemCount: titles.length,
             itemBuilder: (BuildContext, index){
               return Card(
                 child: ListTile(
-
                   tileColor: Colors.green,
                     title: Text(titles[index]),
-                    subtitle: Text(subtitles[index]),
-                    //leading: Image(image: images[index]),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(subtitles[index]),
+                        Text(subtitles2[index]),
+                      ],
+                    ),
+                    leading: Image(image: NetworkImage(images[index],scale:1.1)),
                 ),
               );
             }),
